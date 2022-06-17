ab10a6d76e14705fa3de9db0188195a2b26063ee
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-06-17 09:02:11 +0100

    upgrade

--- test/Boxtest.ts
+++ test/Boxtest.ts
@@ -9,18 +9,28 @@ describe("Box", function(){
     beforeEach(async function(){
         const Box = await ethers.getContractFactory("Box")
         box = await upgrades.deployProxy(Box, [42], {initializer: 'store'})
-        await box.deployed()
+        //await box.deployed()
     })
 
     it("should retrieve value previously stored", async function(){
-        const b = await box.store(42)
-        expect(await box.retrieve()).to.equal(BigNumber.from("42"))
-        await expect(b).to.emit(box, "ValueChanged").withArgs(42);
+
+        // TEST FOR FIRST DEPLOYMENT
+
+        // const b = await box.store(42)
+        // expect(await box.retrieve()).to.equal(BigNumber.from("42"))
+        // await expect(b).to.emit(box, "ValueChanged").withArgs(42);
 
 
-        const _b = await box.store(100)
-        expect(await box.retrieve()).to.equal(BigNumber.from("100"))
-        await expect(_b).to.emit(box, "ValueChanged").withArgs(100);
+        // const _b = await box.store(100)
+        // expect(await box.retrieve()).to.equal(BigNumber.from("100"))
+        // await expect(_b).to.emit(box, "ValueChanged").withArgs(100);
+
+        // TEST FOR DEPLOYED CONTRACT
+
+        expect(await box.retrieve()).to.equal(BigNumber.from('42'))
+
+        await box.store(100)
+        expect(await box.retrieve()).to.equal(BigNumber.from('100'))
     })
 
 })
